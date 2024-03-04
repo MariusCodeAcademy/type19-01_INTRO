15f8e3c8e73dd97b8b62702e2d02f11e1f29c96d
DovydasMar <mardovydas@gmail.com>
2024-03-04 09:17:46 +0200

    funkcija

--- dovydasM/index.js
+++ dovydasM/index.js
@@ -1,8 +1,8 @@
 console.log('hello from the other side');
 
-function dovydoFunkcija(nr1, nr2, nr3, nr4, nr5) {
-  const rez = (nr1 + nr2 + nr3 + nr4) * nr5;
+function dovydoFunkcija(nr1, nr2, nr3) {
+  const rez = nr1 + nr2 + nr3;
   console.log('rez ===', rez);
   return rez;
 }
-dovydoFunkcija(2, 3, 4, 5, 0);
+dovydoFunkcija(2, 3, 4);
