a31b2549d09e152d5976db57dd44e527648f0235
RokasV <vezikauskas.r@gmail.com>
2024-03-04 09:49:18 +0200

    fn rokas sum added

--- public/index2.js
+++ public/index2.js
@@ -1,3 +1,10 @@
 console.log('index2.js');
 
 // cia sukeliam savo funkcijas
+
+// rokas fn start
+function sum(a, b, c) {
+  console.log(`skaiciu suma yra ${a + b + c}`);
+  return a + b + c;
+}
+// rokas fn end
