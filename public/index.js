3c26ca2eb337bcdd4c88ddcab884ef30bfdc7206
SarunasT <sarunas.tarvydas@hotmail.com>
2024-03-04 09:35:48 +0100

    sum new changes

--- public/index.js
+++ public/index.js
@@ -2,3 +2,9 @@ console.log('index.js');
 
 // cia sukeliam savo funkcijas
 
+// rokas fn start
+function sum(a, b, c) {
+  console.log(`skaiciu suma yra ${a + b + c}`);
+  return a + b + c;
+}
+// rokas fn end
--- public/index2.js
+++ public/index2.js
@@ -1,10 +1,3 @@
 console.log('index2.js');
 
 // cia sukeliam savo funkcijas
-
-// rokas fn start
-function sum(a, b, c) {
-  console.log(`skaiciu suma yra ${a + b + c}`);
-  return a + b + c;
-}
-// rokas fn end
