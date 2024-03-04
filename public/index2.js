c8111a21057fb6e23bc767927abae90083cac6f3
Sakalis <lsverslas@gmail.com>
2024-03-04 09:56:04 +0200

    Fun

--- public/index2.js
+++ public/index2.js
@@ -1,3 +1,10 @@
 console.log('index2.js');
 
 // cia sukeliam savo funkcijas
+function trisSkaicius(a, b, c) {
+    if (a + b > c && a + c > b && b + c > a) {
+        return true;
+    } else {
+        return false;
+    }
+}
