10b68cf59bbbd78f6f788d9295b694253a19fb5f
SAudrius <121971874+SAudrius@users.noreply.github.com>
2024-03-04 09:52:57 +0200

    build(fn): move function to global function file

--- public/index.js
+++ public/index.js
@@ -1,3 +1,10 @@
 console.log('index.js');
 
 // cia sukeliam savo funkcijas
+
+// audrius fn start
+const calcMultiplyArr = (numArr) => {
+  const result = numArr.reduce((acu, num) => num * acu);
+  return result;
+};
+// audrius fn end
