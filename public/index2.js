686a4573e4fa3f58acbaa04be4e63fd8fdd6f030
Simona Sud <ssimona.sud@gmail.com>
2024-03-04 09:49:09 +0200

    function multi

--- public/index2.js
+++ public/index2.js
@@ -1,3 +1,15 @@
 console.log('index2.js');
 
 // cia sukeliam savo funkcijas
+
+// simona fn start
+function sudetiAtimti(num1, num2, num3) {
+  const suma = num1 + num2;
+  let rez = suma - num3;
+
+  console.log('suma ===', suma);
+  console.log('rez ===', rez);
+
+  return rez;
+}
+// simona fn end
--- simonas/index.js
+++ simonas/index.js
@@ -2,6 +2,7 @@
 
 // pasidaryti funkcija kuri sudeda du pirmus skaicius ir tada atima trecia skaiciu, atspauzdina ir grazina rezultata
 
+// simona fn start
 function sudetiAtimti(num1, num2, num3) {
   const suma = num1 + num2;
   let rez = suma - num3;
@@ -11,6 +12,7 @@ function sudetiAtimti(num1, num2, num3) {
 
   return rez;
 }
+// simona fn end
 
 let rez = sudetiAtimti(11, 6, 4);
 console.log('Rezultatas:', rez);
