a90b1db3c4f3247d5b0059487bbca17b08b58546
Aleksandra <aleksandrachvatovaite@gmail.com>
2024-03-05 19:49:57 +0200

    mobile

--- _elegantShop/src/components/UI/Button.tsx
+++ _elegantShop/src/components/UI/Button.tsx
@@ -5,7 +5,7 @@ type ButtonProps = {
 
 export default function Button({ children }: ButtonProps) {
   return (
-    <button className="">
+    <button className="bg-slate-950 text-white text-xs px-14 py-4 rounded-lg">
       {children}
     </button>
   );
