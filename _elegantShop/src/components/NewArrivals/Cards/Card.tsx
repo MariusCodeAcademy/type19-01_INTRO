1ac96ee3ae005ba51946ec88a9b6f3c869f3fb4b
TadasJ <tadasjakovlevas@gmail.com>
2024-03-05 12:00:48 +0200

    New Arrivals mobile

--- _elegantShop/src/components/NewArrivals/Cards/Card.tsx
+++ _elegantShop/src/components/NewArrivals/Cards/Card.tsx
@@ -1,10 +1,11 @@
 type CardProps = {
   img: string;
+  price: string;
 };
 
-export default function Card({ img }: CardProps) {
+export default function Card({ img, price }: CardProps) {
   return (
-    <div className='min-w-[231px]  h-[412px]'>
+    <div className='min-w-[231px]  mb-8 h-[412px]'>
       <div className='relative group'>
         <img className='' src={img} alt='' />
         <button className='border absolute top-3 right-3 hidden group-hover:block p-1 rounded-full bg-white'>
@@ -13,6 +14,7 @@ export default function Card({ img }: CardProps) {
         <button className='top-60 left-3 hidden group-hover:block w-[202px] absolute h-10  bg-black text-white rounded-xl'>
           Add to cart
         </button>
+        <span className='bg-white absolute top-3 left-3 py-1 px-4 text-sm  leading-4 '>NEW</span>
         <div className='flex mt-3 mb-1'>
           <span>
             <img src='/public/HomePageImages/Star Icon.png' alt='star icon' />
@@ -32,7 +34,7 @@ export default function Card({ img }: CardProps) {
         </div>
         <div>
           <h1 className='text-base fw-semibold'>Skullcandy - Crusher anc 2 wireless headphones</h1>
-          <p className='mt-1 text-sm fw-semibold'>$299.99</p>
+          <p className='mt-1 text-sm fw-semibold'>{price}</p>
         </div>
       </div>
     </div>
