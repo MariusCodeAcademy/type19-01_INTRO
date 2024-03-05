96770b319396366151f4b23e694f7dc401b804dd
SarunasT <sarunas.tarvydas@hotmail.com>
2024-03-05 11:05:01 +0100

    pridejau i home page savo darba

--- _elegantShop/src/App.tsx
+++ _elegantShop/src/App.tsx
@@ -2,7 +2,6 @@
 
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import BestSellerPage from './components/bestSellerComp/BestSellerPage';
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />}></Route>
       </Routes>
-      <BestSellerPage />
     </div>
   );
 }
--- _elegantShop/src/pages/HomePage.tsx
+++ _elegantShop/src/pages/HomePage.tsx
@@ -1,10 +1,13 @@
 //
 
+import BestSellerPage from '../components/bestSellerComp/BestSellerPage';
+
 function HomePage() {
   return (
     <div>
       <h1 className='text-2xl'>Hero</h1>
       <h1>Slider</h1>
+      <BestSellerPage />
     </div>
   );
 }
