b63165893963b3aa2e05a7eb81bb8a7ce41f53a7
Simona Sud <ssimona.sud@gmail.com>
2024-03-05 12:53:34 +0200

    init

--- _elegantShop/src/pages/HomePage.tsx
+++ _elegantShop/src/pages/HomePage.tsx
@@ -1,10 +1,13 @@
 //
 
+import Banner from '../components/Banner';
+
 function HomePage() {
   return (
     <div>
       <h1 className='text-2xl'>Hero</h1>
       <h1>Slider</h1>
+      <Banner />
     </div>
   );
 }
