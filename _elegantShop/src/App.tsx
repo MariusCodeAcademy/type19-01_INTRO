5c2727f0433407893c3938f6e28335bbc2aab8ee
DovydasMar <mardovydas@gmail.com>
2024-03-05 13:00:33 +0200

    desktop done

--- _elegantShop/src/App.tsx
+++ _elegantShop/src/App.tsx
@@ -2,11 +2,12 @@
 
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className=''>
-      {/* Header */}
+      <Header />
       <Routes>
         <Route path='/' element={<HomePage />}></Route>
       </Routes>
