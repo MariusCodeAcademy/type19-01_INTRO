d79cd761650df612f7f9e34adce42b4fe4606fc1
RokasV <vezikauskas.r@gmail.com>
2024-03-05 11:33:10 +0200

    brands desktop done

--- _elegantShop/src/components/Brands.tsx
+++ _elegantShop/src/components/Brands.tsx
@@ -3,7 +3,7 @@ import OneBrand from './UI/OneBrand';
 function Brands() {
   return (
     <div>
-      <div className='flex justify-start pl-8 gap-4 my-6 overflow-hidden flex-shrink-0 sm:justify-center'>
+      <div className='flex justify-start pl-8 gap-4 my-6 overflow-hidden flex-shrink-0 sm:justify-center lg:gap-6 lg:my-10'>
         <OneBrand src='/images/logo 01.svg' alt='Brand 1' />
         <OneBrand src='/images/logo 02.svg' alt='Brand 2' />
         <OneBrand src='/images/logo 03.svg' alt='Brand 3' />
