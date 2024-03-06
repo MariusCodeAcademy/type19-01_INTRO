1c92390ac6d2e181b486a21b6b352ed1b7d7cff2
Sakalis <lsverslas@gmail.com>
2024-03-06 10:48:12 +0200

    Deskas

---
+++ _elegantShop/src/components/DesktopFooter.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './DesktopFooter.css';
+
+const DesktopFooter: React.FC = () => {
+    return (
+        <footer className="desktop-footer">
+            <div className="logo">Headphone Store</div>
+            <div className="links">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Shop</a></li>
+                    <li><a href="#">Product</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </div>
+            <div className="social-media">
+                <a href="#"><img src="instagram.png" alt="Instagram" /></a>
+                <a href="#"><img src="facebook.png" alt="Facebook" /></a>
+                <a href="#"><img src="youtube.png" alt="YouTube" /></a>
+            </div>
+            <div className="legal">
+                <ul>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Terms of Use</a></li>
+                </ul>
+            </div>
+            <div className="copyright">
+                Copyright © 2023 3iegant. All rights reserved.
+            </div>
+        </footer>
+    );
+}
+
+export default DesktopFooter;
--- _elegantShop/src/pages/HomePage.tsx
+++ _elegantShop/src/pages/HomePage.tsx
@@ -1,10 +1,24 @@
-//
+import { ShopCollection } from '../components/homePage/ShopCollection/ShopCollection';
+
+import NewArivals from '../components/homePage/NewArivals';
+
+import BestSellerPage from '../components/bestSellerComp/BestSellerPage';
+import TimerST from '../components/TimerST/TimerST';
+
+import Hero from '../components/homePage/Hero';
+
+import Banner from '../components/Banner';
 
 function HomePage() {
   return (
     <div>
-      <h1 className='text-2xl'>Hero</h1>
+      <Hero />
       <h1>Slider</h1>
+      <BestSellerPage />
+      <Banner />
+      <NewArivals />
+      <ShopCollection />
+      <TimerST />
     </div>
   );
 }
