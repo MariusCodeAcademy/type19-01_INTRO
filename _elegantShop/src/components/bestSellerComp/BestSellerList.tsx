b7ba9fff6577a1e0eec455dc739586b3f30dab8d
SarunasT <sarunas.tarvydas@hotmail.com>
2024-03-07 14:20:08 +0100

    map

--- _elegantShop/src/components/bestSellerComp/BestSellerList.tsx
+++ _elegantShop/src/components/bestSellerComp/BestSellerList.tsx
@@ -5,7 +5,13 @@ import OneCardBestSeller from './OneCardBestSeller';
 export default function BestSellerList() {
   return (
     <div className='mb-5 grid grid-cols-2 md:grid-cols-4 mt-1 gap-2'>
-      <OneCardBestSeller name='Sonny - WH-1000XM...' price={299.99} soldPrice={200.25} img='/public/imgBest Seller/198f8a151916c6fc9755e6971e67962d.png' />
+      {/* <ul>
+        {Array.map((sObj) => {
+          <li key={sObj.id}>
+            <OneCardBestSeller name={sObj.name} price={sObj.price} soldPrice={sObj.soldOut} img={sObj.img} />
+          </li>;
+        })}
+      </ul> */}
 
       <OneCardBestSeller name='Sonny - WH-1000XM...' price={299.99} img='/imgBest Seller/a3b1564d603be3ebdd3d781e7db79590.png' />
       <OneCardBestSeller name='Sonny - WH-1000XM...' price={299.99} img='/imgBest Seller/b628dddf967efb911899b4f87fe5b2ef.png' />
@@ -16,10 +22,6 @@ export default function BestSellerList() {
       <OneCardBestSeller name='Sonny - WH-1000XM...' price={299.99} img='/imgBest Seller/b628dddf967efb911899b4f87fe5b2ef.png' />
       <OneCardBestSeller name='Sonny - WH-1000XM...' price={299.99} img='/imgBest Seller/6.png' />
       <OneCardBestSeller name='Sonny - WH-1000XM...' price={299.99} soldPrice={200.25} img='/imgBest Seller/198f8a151916c6fc9755e6971e67962d.png' />
-
-      <OneCardBestSeller name='Sonny - WH-1000XM...' price={299.99} img='/imgBest Seller/a3b1564d603be3ebdd3d781e7db79590.png' />
-      <OneCardBestSeller name='Sonny - WH-1000XM...' price={299.99} img='/imgBest Seller/b628dddf967efb911899b4f87fe5b2ef.png' />
-      <OneCardBestSeller name='Sonny - WH-1000XM...' price={299.99} img='/imgBest Seller/d2eda88ccb4cc71f499a5d94c2a5bd61.png' />
     </div>
   );
 }
--- _elegantShop/src/components/bestSellerComp/OneCardBestSeller.tsx
+++ _elegantShop/src/components/bestSellerComp/OneCardBestSeller.tsx
@@ -10,7 +10,7 @@ type BestSellerProps = {
 
 export default function OneCardBestSeller({ name, price, soldPrice, img }: BestSellerProps) {
   return (
-    <div className=''>
+    <div className='mb-10 mt-5'>
       <div className='Elements relative'>
         <img className='bg-red-50 object-contain w-full h-60' src={img} alt='sony casque' />
         <div className='pill absolute top-4 left-4 px-3 py-1  bg-white rounded'>
@@ -20,11 +20,6 @@ export default function OneCardBestSeller({ name, price, soldPrice, img }: BestS
       <div className='content mt-2'>
         <div className='flex gap-1 '>
           <RatingsST />
-          {/* <img className='h-4 w-4' src='/public/Star Fill.png' alt='star icon' />
-          <img className='h-4 w-4' src='/public/Star Fill.png' alt='star icon' />
-          <img className='h-4 w-4' src='/public/Star Fill.png' alt='star icon' />
-          <img className='h-4 w-4' src='/public/Star Fill.png' alt='star icon' />
-          <img className='h-4 w-4' src='/public/Star Fill.png' alt='star icon' /> */}
         </div>
         <h3 className='text-sm font-semibold mt-1 mb-1'>{name}</h3>
         <PricingTs price={price} soldPrice={soldPrice} />
