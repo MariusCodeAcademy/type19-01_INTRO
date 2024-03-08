b84abb058b59cb6304bad10299e20dc387c6a9ab
SAudrius <121971874+SAudrius@users.noreply.github.com>
2024-03-08 11:25:29 +0200

    build(hero): build blog hero for mobile and desktop

--- _elegantShop/src/pages/BlogPage.tsx
+++ _elegantShop/src/pages/BlogPage.tsx
@@ -1,9 +1,11 @@
+import { BlogHero } from '../components/blogPage/BlogHero/BlogHero';
 import Header from '../components/header/Header';
 
 function BlogPage() {
   return (
     <div>
       <Header />
+      <BlogHero />
     </div>
   );
 }
