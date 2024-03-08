ad3b2adcbacfc74efd57652ff975a778f0c6b869
RokasV <vezikauskas.r@gmail.com>
2024-03-08 10:12:45 +0200

    card done

--- _elegantShop/src/pages/BlogPage.tsx
+++ _elegantShop/src/pages/BlogPage.tsx
@@ -1,9 +1,13 @@
+import OneBlogCard from '../components/blogPage/OneBlogCard';
 import Header from '../components/header/Header';
 
 function BlogPage() {
   return (
     <div>
       <Header />
+      <OneBlogCard img='/img/antras.jpg' title='Blog 1' date='2021-10-10' />
+      <OneBlogCard img='/img/antras.jpg' title='Blog 1' date='2021-10-10' />
+      <OneBlogCard img='/img/antras.jpg' title='Blog 1' date='2021-10-10' />
     </div>
   );
 }
