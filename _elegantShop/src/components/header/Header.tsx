e439f9383ffc858faa67fae2095aba9355936f0e
MariusMokymai <testams752@gmail.com>
2024-03-08 11:33:29 +0200

    meniuItems array + map to generate nav

--- _elegantShop/src/components/header/Header.tsx
+++ _elegantShop/src/components/header/Header.tsx
@@ -3,6 +3,37 @@ import HeaderMenu from './HeaderMenu';
 import { NavLink } from 'react-router-dom';
 // import HeaderMenu from './HeaderMenu';
 
+type menuItemType = {
+  id: number;
+  url: string;
+  name: string;
+  children?: menuItemType[];
+};
+
+const meniuItems: menuItemType[] = [
+  { id: 1, url: '/', name: 'Home' },
+  {
+    id: 2,
+    url: '/shop',
+    name: 'Shop',
+    children: [
+      { id: 1, url: '/shop/new', name: 'New Items' },
+      { id: 2, url: '/shop/popular', name: 'Popular Items' },
+    ],
+  },
+  {
+    id: 3,
+    url: '/product',
+    name: 'Products',
+    children: [
+      { id: 1, url: '/product/1', name: 'Sofa' },
+      { id: 2, url: '/product/2', name: 'Dalykas' },
+    ],
+  },
+  { id: 4, url: '/contact-us', name: 'Contact Us' },
+  { id: 5, url: '/blog', name: 'Blog' },
+];
+
 type HeaderProps = {
   main?: boolean;
 };
@@ -18,10 +49,7 @@ export default function Header({ main }: HeaderProps) {
       {menuClicked && <HeaderMenu onMenuClick={handleMenuClick} />}
       {!menuClicked && (
         <header
-          className={`flex z-50 relative  px-8 py-4 justify-between ${
-            main ? 'bg-main' : ''
-          } `}
-        >
+          className={`flex z-50 relative  px-8 py-4 justify-between ${main ? 'bg-main' : ''} `}>
           <div className='flex '>
             <img
               onClick={handleMenuClick}
@@ -34,36 +62,22 @@ export default function Header({ main }: HeaderProps) {
             </p>
           </div>
           <div id='hiddenOnSmall' className='md:flex hidden sm:flex'>
+            {/* main menu items */}
             <ul className='flex items-center gap-4'>
-              <li className='  font-semibold'>
-                <NavLink to={'/'}>Home</NavLink>
-              </li>
-              <li className='  font-semibold flex justify-between'>
-                Shop{' '}
-                <img
-                  src='/img/header/arrow-down.svg'
-                  alt='view more shop items'
-                ></img>
-              </li>
-              <li className='  '>
-                <NavLink
-                  to={'/shop/1'}
-                  className='font-semibold flex justify-between'
-                >
-                  Product{' '}
-                  <img
-                    src='/img/header/arrow-down.svg'
-                    alt='view more shop items'
-                  ></img>
-                </NavLink>
-              </li>
-              <li className='  font-semibold'>
-                <NavLink to={'/contact-us'}>Contact Us</NavLink>
-              </li>
-              <li className='  font-semibold'>
-                <NavLink to={'/blog'}>Our Blog</NavLink>
-              </li>
+              {meniuItems.map((mObj) => (
+                <li key={mObj.id} className='font-semibold'>
+                  <NavLink
+                    to={mObj.url}
+                    className={mObj.children && mObj.children.length ? 'flex justify-between' : ''}>
+                    {mObj.name}{' '}
+                    {mObj.children ? (
+                      <img src='/img/header/arrow-down.svg' alt='view more shop items'></img>
+                    ) : null}
+                  </NavLink>
+                </li>
+              ))}
             </ul>
+            {/* // main menu items */}
           </div>
           <div className='flex gap-1'>
             <img src='/img/header/shoppingbag.svg' alt='shopping cart' />
